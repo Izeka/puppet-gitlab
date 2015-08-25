e01102dea555c1429410eb13d13aec978c6af1de
izeka <izeka1983@hotmail.com>
2015-08-25 10:45:16 -0300

    add gitlab_repo variable

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,15 +1,20 @@
-class gitlab::install{
+class gitlab::install(
+ $gitlab_script = 'https://packages.gitlab.com/install/repositories/gitlab/gitlab-ce/script.rpm.sh',
+ $repo_directory= "/etc/yum.repo.d",
+ $gitlab_repo = "$repo_directory/gitlab_gitlab-ce.repo"
+){
     
     package{'gitlab-ce':
         ensure => present,
 	allow_virtual => false,
 	require => Exec["get repository"]
     }
-    package{'deltarpm','crontabs':
+    package{['deltarpm','crontabs']:
 	ensure => present,
 	allow_virtual => false,
     }
     exec{'get repository':
-	command => '/bin/curl https://packages.gitlab.com/install/repositories/gitlab/gitlab-ce/script.rpm.sh |  /bin/bash',
+	command => "/bin/curl $gitlab_script |  /bin/bash",
+	creates => "$gitlab_repo"
     }
 }
